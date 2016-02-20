5364c33ec790e9ff683794fe635702847cbf8793
Sahil <sahilshah@flash.net>
2016-02-20 15:41:28 -0600

    changes

--- A3/src/Assignment3/A3Driver.java
+++ A3/src/Assignment3/A3Driver.java
@@ -1,6 +1,10 @@
 package Assignment3;
 
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class A3Driver 
 	{
@@ -15,6 +19,9 @@ public class A3Driver
 		 *  		operations	
 		 * */
 		  
+		
+		  ArrayList<String> fileinput = new ArrayList<String>();
+		  fileinput = file(args[0]);
 		  
 		// TODO Auto-generated method stub
 		
@@ -49,8 +56,83 @@ public class A3Driver
 		}		
 	  }
 	  
-	  static void insert(){ //yash
+	  
+	  /*********************************************************************************
+	  * Method Name: insert                                                            
+	  * Purpose: takes a new item and inserts it into the arraylist(your shopping cart)
+	  *          in alphabetical order. It can add 2 of the same elements, so that     
+	  *          there can be 2 different hats      									   
+	  * Returns: None                                                                  
+	  * 																			   
+	  * questions: 1. if it passes in an existing item should i just add it in again or   
+	  * 		  update the current item quantity. what if the prices are different?  
+	  * 
+	  * 		 2. is Hat the same as hat? i did a toupperCase when comparing the strings
+	  * 			
+	  ******************************************************************************/	  
+	  static void insert(ArrayList<Item> shoppingCart, Item input){ //yash
+		  Iterator<Item> i = shoppingCart.iterator();
+		  int x = 0;
+		  boolean added = false;
+		  String inputname = input.getName();
+		  inputname = inputname.toUpperCase();
 		  
+			while (i.hasNext() && !added) 
+			{
+				boolean incremented = false;
+				Item temp = i.next();
+				String name = temp.getName();
+				name = name.toUpperCase();
+				
+////////////if the shoppingcart word's first letter is greater than inputname's
+//////////first letter then add
+				if(name.charAt(0) > inputname.charAt(0))
+				{
+					shoppingCart.add(x, input);
+					added = true;
+					System.out.println("The item " + input.getName()+ " was added to the shoppingcart");
+				}
+				
+///////if first letter less than input's first letter then increment x
+				if(name.charAt(0) < inputname.charAt(0))
+				{
+					x++;
+					incremented = true;
+				}
+				
+				
+////////////in case they have the same beginning letters, for example shirts and shorts
+				if(name.charAt(0)==inputname.charAt(0))
+				{
+					for(int y = 1; y<name.length();y++)
+					{
+						
+						if( y<inputname.length() && !added)
+						{
+							if( (name.charAt(y)>inputname.charAt(y)) )
+							{
+								shoppingCart.add(x, input);
+								added = true;
+								System.out.println("The item " + input.getName()+ " was added to the shoppingcart");
+							}
+						}
+					}
+				}
+				
+///////////increment x if same beginning but shoppingcart name should come before input name
+				if(!added && !incremented)
+				{
+					x++;
+				}
+			}
+
+/////in case the item to be added is supposed to be the last entry in arraylist
+			if(!added)
+			{
+				shoppingCart.add(shoppingCart.size(), input);
+				added = true;
+				System.out.println("The item " + input.getName()+ " was added to the shoppingcart");
+			}
 	  }
 	  
 	  /******************************************************************************
@@ -107,8 +189,34 @@ public class A3Driver
 		  System.out.print(delete + " " + input + " " + "item/items were/was deleted.");
 	  }
 	  
-	  static void update(){ //yash
+	  
+	  /******************************************************************************
+	  * Method Name: update                                                         
+	  * Purpose: input item and quantity change and it will update the quantity
+	  * 		 as long as the change you want to make is >0.                                                          
+	  * Returns: None                                               
+	  * 
+	  * questions: what if current item quantity = 3 but they want to take 1 out?
+	  * 			would -1 be a legal input
+	  ******************************************************************************/  
+	  static void update(ArrayList<Item> shoppingCart, Item input, int change){ //yash
+		  Iterator<Item> i = shoppingCart.iterator();
 		  
+		  if(change>0)
+		  {
+			  while (i.hasNext()) 
+			  {
+				  Item temp = i.next();
+				  
+				  if(temp.getName().equals(input.getName()))
+				  {
+					  int quantity = temp.getQuantity();
+					  quantity = quantity + change;
+					  temp.setQuantity(quantity);
+					  System.out.println("The item " + temp.getName()+" had "+change+" added to its quantity");
+				  }
+			  }
+		  }
 	  }
 	  
 	  /******************************************************************************
@@ -134,10 +242,20 @@ public class A3Driver
 			System.out.print("Total Shopping Cart Price: $" + cartprice);
 	  }
 	  
-	  static String[] input(){ //yash
-		return null;  //parse the string into an array
-		
+      /******************************************************************************
+	  * Method Name: input                                                         
+	  * Purpose: takes the whole input line string(comes from read file function)
+	  * 		   and splits it into different words which are put in an array of 
+	  * 		   strings without the spaces                                                               
+	  * Returns: array of strings with separate words                                                                
+	  ******************************************************************************/		  
+	  static String[] input(String input){ //yash
+			  
+		  String[] inputs = input.split("\\s+");  
+		  return inputs;  
+			
 	  }
+	  
 	  
 	  /******************************************************************************
 	  * Method Name: checkErrors                                                    *
@@ -246,11 +364,48 @@ public class A3Driver
 		  }
 	  }
 	  
-	  static String file(){ //yash
+	  
+	  /******************************************************************************
+	   * Method Name: file                                                         
+	   * Purpose: opens the file and then reads each line until there are no
+	   * 		   more lines to read and stores each line as its own
+	   *           element in an arraylist.                                                            
+	   * Returns: arraylist of every line in the input file, each line is
+	   * 		  it's own element in arraylist                                                               
+	   ******************************************************************************/
+	  static ArrayList<String> file(String filename) //yash
+	  {
+		  
+		  ArrayList<String> inputlines = new ArrayList<String>();
+		  
+		  try 
+			{
+				FileReader freader = new FileReader(filename);
+				BufferedReader reader = new BufferedReader(freader);
+				
+				for (String s = reader.readLine(); s != null; s = reader.readLine()) 
+				{
+					inputlines.add(s);
+				}
+		
+				reader.close();
+				return inputlines;
+			} 
+			catch (FileNotFoundException e) 
+			{
+				System.err.println ("Error: File not found. Exiting...");
+				e.printStackTrace();
+				System.exit(-1);
+			} catch (IOException e) 
+			{
+				System.err.println ("Error: IO exception. Exiting...");
+				e.printStackTrace();
+				System.exit(-1);
+			}
+		  
+		  
 		  return null;
 	  }
-	  
-	  
 	  
 	  
 
